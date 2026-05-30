707e5052f2d4ccec7a296ac336eeb9f186a9a71e
million <millionsilishe@gmail.com>
2026-05-31 01:00:26 +0300

    Even or Odd

---
+++ even_odd.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  int num;
+  cout<<"Please Enter A number: ";
+  cin>>num;
+
+  if (num%2==0)
+    cout<<"Even"<<endl;
+  else
+    cout<<"odd"<<endl;
+  return 0;
+}
