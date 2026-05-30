228247f007a3391b283588188968393abbb5a349
million <millionsilishe@gmail.com>
2026-05-31 00:39:57 +0300

    another option to swaps

--- swaps.cpp
+++ swaps.cpp
@@ -8,13 +8,13 @@ int main ()
 	cin>>x;
 	cout<<"Enter the value of y: ";
 	cin>>y;
-
+/*
 	temp = x;
 	x = y;
 	y = temp;
-
-	cout<<"The value of x is "<<x<<endl;
-	cout<<"The Value of y is "<<y<<endl;
+*/
+	cout<<"The value of x is "<<y<<endl;
+	cout<<"The Value of y is "<<x<<endl;
 
 	return 0;
 }
