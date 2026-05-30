0723ef8c4002b9e4f7edff2e49a04b7ede534786
million <millionsilishe@gmail.com>
2026-05-31 00:02:23 +0300

    Area & circumference of circle calculater

---
+++ area_circumference.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+			/* radius = r
+			   area = a
+			   circumference = c */
+	float a, r, c;
+	cout<<"Enter the radius of the circle: ";
+	cin>>r;
+
+	a = r*r*3.14;
+
+	cout<<"Area = "<<a<<endl;
+	cout<<"Circumferance = "<<c;
+return 0;
+
+}
